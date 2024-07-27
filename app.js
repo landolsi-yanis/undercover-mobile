9a029c74c57c383c25e8bcd6eecafeaa844b43df
landolsi-yanis <landolsiyanis@gmail.com>
2024-07-28 00:27:13 +0200

    Added Sessions Class

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+console.log("Motherfuckiing");
+
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});
+
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
+});
---
+++ session.js
@@ -0,0 +1,8 @@
+class Session {
+    constructor(players, leaderboard, gameNb, hasMrWhite) {
+        this.players = players;
+        this.leaderboard = leaderboard;
+        this.gameNb = gameNb;
+        this.hasMrWhite = hasMrWhite;
+      }
+  }
